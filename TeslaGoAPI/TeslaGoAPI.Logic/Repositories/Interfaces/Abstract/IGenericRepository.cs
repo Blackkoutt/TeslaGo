38197722f7b94c4106c992987ef1d87fd3539725
Mateusz Strapczuk <mateusz.strapczuk@gmail.com>
2025-04-05 20:31:07 +0200

    Adding Generic Repository

---
+++ TeslaGoAPI/TeslaGoAPI.Logic/Repositories/Interfaces/Abstract/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace TeslaGoAPI.Logic.Repositories.Interfaces.Abstract
+{
+    public interface IGenericRepository<TEntity>
+    {
+        Task<TEntity?> GetOneAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>>? query = null);
+        Task Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        void Detach(TEntity entity);
+    }
+}
---
+++ TeslaGoAPI/TeslaGoAPI.Logic/Repositories/Repositories/Abstract/GenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+using TeslaGoAPI.DB.Context;
+using TeslaGoAPI.Logic.Repositories.Interfaces.Abstract;
+
+namespace TeslaGoAPI.Logic.Repositories.Repositories.Abstract
+{
+    public abstract class GenericRepository<TEntity>(APIContext context) : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly APIContext _context = context;
+        private readonly DbSet<TEntity> _table = context.Set<TEntity>();
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>>? query = null)
+        {
+             return await(query?.Invoke(_table) ?? _table).ToListAsync();
+        }
+        public virtual async Task<TEntity?> GetOneAsync(int id) => await _table.FindAsync(id);
+        public async Task Add(TEntity entity) => await _table.AddAsync(entity);
+        public void Delete(TEntity entity) => _table.Remove(entity);
+        public void Update(TEntity entity) => _table.Update(entity);
+        public void Detach(TEntity entity) => _table.Entry(entity).State = EntityState.Detached;
+    }
+}
